8366ca4ccb094e912c52ce1b9ee6170cf50205ff
ahneo <neohenry@gmail.com>
2019-11-06 15:26:24 +0800

    crud fruits example

---
+++ unit_2/w11d2/instructor_examples/crudFruits/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Fruit = require('./models/Fruit');
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+//... and then farther down the file
+mongoose.connect('mongodb://localhost:27017/fruitscrud', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
+});
+
+// Index route
+app.get('/fruits', (req, res) => {
+	Fruit.find({}, (err, allFruits) => {
+		if (err) console.error(err.message);
+		else {
+			res.render('index.ejs', {
+				fruits: allFruits
+			});
+		}
+	});
+});
+
+// API Index route
+app.get('/api/fruits', (req, res) => {
+	Fruit.find({}, (err, allFruits) => {
+		if (err) console.error(err.message);
+		else {
+			res.json(allFruits);
+		}
+	});
+});
+
+// New route
+app.get('/fruits/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// Show route
+app.get('/fruits/:id', (req, res) => {
+	Fruit.findById(req.params.id, (err, foundFruit) => {
+		if (err) res.send(err.message);
+
+		res.render('show.ejs', {
+			fruit: foundFruit
+		});
+	});
+});
+
+// Create route
+app.post('/fruits/', (req, res) => {
+	if (req.body.readyToEat === 'on') {
+		//if checked, req.body.readyToEat is set to 'on'
+		req.body.readyToEat = true;
+	} else {
+		//if not checked, req.body.readyToEat is undefined
+		req.body.readyToEat = false;
+	}
+
+	Fruit.create(req.body, (err, createdFruit) => {
+		if (err) res.send(err.message);
+
+		res.redirect('/fruits');
+	});
+});
+
+// Server listening
+app.listen(3000, () => {
+	console.log('listening');
+});
