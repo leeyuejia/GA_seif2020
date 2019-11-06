2ca52f6ecff8fede06a3f4aa6c67dbf6e21455f5
ahneo <neohenry@gmail.com>
2019-11-06 20:53:58 +0800

    update instructor examples for w11d2 and w11d3

--- unit_2/w11d2/instructor_examples/crudFruits/server.js
+++ unit_2/w11d2/instructor_examples/crudFruits/server.js
@@ -1,76 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Fruit = require('./models/Fruit');
+const methodOverride = require('method-override');
+
+const fruitsController = require('./controllers/fruits.js');
 
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// Use Controllers and Routes
+app.use('/fruits', fruitsController);
 
 //Connect to database
 //mongodb://localhost:27017/fruitscrud
 //mongodb+srv://henry:<password>@cluster0-dqzcz.mongodb.net/fruitscrud?retryWrites=true&w=majority
-mongoose.connect('mongodb://localhost:27017/fruitscrud', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+mongoose.connect(
+	'mongodb+srv://henry:henry@cluster0-dqzcz.mongodb.net/fruitscrud?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
 mongoose.connection.once('open', () => {
 	console.log('connected to mongo');
-});
-
-// Index route
-app.get('/fruits', (req, res) => {
-	Fruit.find({}, (err, allFruits) => {
-		if (err) console.error(err.message);
-		else {
-			res.render('index.ejs', {
-				fruits: allFruits
-			});
-		}
-	});
-});
-
-// API Index route
-app.get('/api/fruits', (req, res) => {
-	Fruit.find({}, (err, allFruits) => {
-		if (err) console.error(err.message);
-		else {
-			res.json(allFruits);
-		}
-	});
-});
-
-// New route
-app.get('/fruits/new', (req, res) => {
-	res.render('new.ejs');
-});
-
-// Show route
-app.get('/fruits/:id', (req, res) => {
-	Fruit.findById(req.params.id, (err, foundFruit) => {
-		if (err) res.send(err.message);
-
-		res.render('show.ejs', {
-			fruit: foundFruit
-		});
-	});
-});
-
-// Create route
-app.post('/fruits/', (req, res) => {
-	if (req.body.readyToEat === 'on') {
-		//if checked, req.body.readyToEat is set to 'on'
-		req.body.readyToEat = true;
-	} else {
-		//if not checked, req.body.readyToEat is undefined
-		req.body.readyToEat = false;
-	}
-
-	Fruit.create(req.body, (err, createdFruit) => {
-		if (err) res.send(err.message);
-
-		res.redirect('/fruits');
-	});
 });
 
 // Server listening
