5ff9300ce651fb4bea09215b729268de7fcb0e25
ahneo <neohenry@gmail.com>
2019-11-13 18:45:58 +0800

    updated blog example

--- unit_2/w12d2/instructor_examples/blog/controllers/articles.js
+++ unit_2/w12d2/instructor_examples/blog/controllers/articles.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("../models/articles.js");
 
 router.get("/", (req, res) => {
   Article.find({}, (err, foundArticles) => {
@@ -7,13 +8,26 @@ router.get("/", (req, res) => {
       articles: foundArticles
     });
   });
+
+  // Article.find()
+  //   .populate("author")
+  //   .exec((err, foundArticles) => {
+  //     if (err) console.error(err.message);
+
+  //     if (foundArticles) {
+  //       console.log("Showing all articles..");
+  //       console.log(foundArticles);
+  //       res.render("articles/index.ejs", {
+  //         articles: foundArticles
+  //       });
+  //     }
+  //   });
 });
 
 router.get("/new", (req, res) => {
   res.render("articles/new.ejs");
 });
 
-const Article = require("../models/articles.js");
 //...
 //...farther down the page
 router.post("/", (req, res) => {
@@ -44,22 +58,22 @@ router.put("/:id", (req, res) => {
 
 //avoid this handling /new by placing it towards the bottom of the file
 router.get("/:id", (req, res) => {
-  // Article.findById(req.params.id, (err, foundArticle)=>{
-  // 	res.render('articles/show.ejs', {
-  // 		article: foundArticle
-  // 	});
-  // });
-
-  Article.findById(req.params.id)
-    .populate("author")
-    .exec((error, foundArticle) => {
-      //dynamically switch out any ids with the objects they reference
-      console.log(foundArticle);
-      res.render("articles/show.ejs", {
-        article: foundArticle
-      });
-      //mongoose.connection.close();
+  Article.findById(req.params.id, (err, foundArticle) => {
+    res.render("articles/show.ejs", {
+      article: foundArticle
     });
+  });
+
+  // Article.findById(req.params.id)
+  //   .populate("author")
+  //   .exec((error, foundArticle) => {
+  //     //dynamically switch out any ids with the objects they reference
+  //     console.log(foundArticle);
+  //     res.render("articles/show.ejs", {
+  //       article: foundArticle
+  //     });
+  //     //mongoose.connection.close();
+  //   });
 });
 
 module.exports = router;
