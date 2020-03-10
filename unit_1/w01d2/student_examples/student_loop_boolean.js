ac6b618d91f420f17b2b809ece4319b8a5faaf00
YJ Lee <leeyuejia@gmail.com>
2020-03-10 23:40:13 +0800

    edited loop.js

---
+++ unit_1/w01d2/student_examples/student_loop_boolean.js
@@ -0,0 +1,18 @@
+
+/*let a = "hi";
+let b = "YEP!"
+for (x=0; x<=15; x++) {
+    console.log(a)
+};
+
+for (z=0; z<=30; z++) {
+    console.log(b)
+};
+*/
+let some_num = 9
+let some_numB = Math.random() * 10
+for (let c=0; c<=100; c++) {
+    if (c <= some_numB) {
+        console.log("that is a small number") 
+    };
+};
---
+++ unit_1/w01d3/homework/loop.js
@@ -0,0 +1,33 @@
+/*//loop numbers that logs number 0-99 (ascending)
+for (let i = 0; i<=9; i++) {
+    console.log(i)
+};
+//loop numbers that log number 99-0 (descending)
+for (let x=9; x>=0; x--) {
+    console.log(x)
+    };
+//loop numbers that log even number 0-99 (ascending)
+for (let y=0; y<=10; y += 2) {
+    console.log(y)
+}
+//loop numbers that log even number 99-0 (descending)
+for (let z=10; z>=0; z -= 2) {
+    console.log(z)
+}*/
+//loop numbers that log odd number 0-99 (ascending)
+for (let a=0; a<=10; a++) {
+    if (a%2 == true) {
+        console.log(a)
+    }
+};
+//loop numbers that log odd number 99-0 (descending)
+for (let b=9; b>=0; b--) {
+    if (b%2 == true) {
+        console.log(b)
+    }
+};
+//loop numbers that log number 49-72 (ascending)
+for (let c = 49; c<=72; c++) {
+    console.log(c)
+};
+//
