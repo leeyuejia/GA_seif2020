e8a35b05d4bf1c7e9e4f08423f5122c5d522c0f3
leeyuejia <leeyuejia@gmail.com>
2020-03-12 17:24:35 +0800

    edited homework

--- unit_1/w01d3/homework/loop.js
+++ unit_1/w01d3/homework/loop.js
@@ -1,4 +1,4 @@
-/*//loop numbers that logs number 0-99 (ascending)
+//loop numbers that logs number 0-99 (ascending)
 for (let i = 0; i<=9; i++) {
     console.log(i)
 };
@@ -13,21 +13,21 @@ for (let y=0; y<=10; y += 2) {
 //loop numbers that log even number 99-0 (descending)
 for (let z=10; z>=0; z -= 2) {
     console.log(z)
-}*/
-//loop numbers that log odd number 0-99 (ascending)
-for (let a=0; a<=10; a++) {
-    if (a%2 == true) {
-        console.log(a)
-    }
-};
-//loop numbers that log odd number 99-0 (descending)
-for (let b=9; b>=0; b--) {
-    if (b%2 == true) {
-        console.log(b)
-    }
-};
-//loop numbers that log number 49-72 (ascending)
-for (let c = 49; c<=72; c++) {
-    console.log(c)
-};
-//
+}
+// //loop numbers that log odd number 0-99 (ascending)
+// for (let a=0; a<=10; a++) {
+//     if (a%2 == true) {
+//         console.log(a)
+//     }
+// };
+// //loop numbers that log odd number 99-0 (descending)
+// for (let b=9; b>=0; b--) {
+//     if (b%2 == true) {
+//         console.log(b)
+//     }
+// };
+// //loop numbers that log number 49-72 (ascending)
+// for (let c = 49; c<=72; c++) {
+//     console.log(c)
+// };
+// //
