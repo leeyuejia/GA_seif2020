855051205d189c95b021279e7694ac95c9fa7a3e
YJ Lee <leeyuejia@gmail.com>
2020-03-21 13:59:08 +0800

    added more student lab stuff

---
+++ unit_1/w03d1/student_labs/pseudocode.js
@@ -0,0 +1,32 @@
+// write a pseudocode for waffles. 
+// let waffle maker machine power to be true
+// let frozenwaffles to be laid on the table
+// if syrup = maple syrup, remove from refridgerator
+// fronzenwaffles += maples syrup
+// 
+
+// peanut butter and jelly sandwich
+// first do we have peanut butter and jam and bread on the table?
+// if we not have the above items, search for the items 
+// take two slices of bread and let it be on the chopping board
+// Check if the peanut butter is open? if it is return true, else return false.
+// search for a spoon, if found, spoon += peanut butter. 
+// let twoscoop of peanut butter = peanutbutter on the spoon
+// add the twoscoop of peanut butter unto the breadsliceA.
+// let jamscoop = jam +=spoon.
+// add jamscoop on breadsliceB. 
+// let peanutbutterjellysandwich = breadsliceA + breadsliceB
+
+//fibonacci number
+// num1 = 0
+// num2 = 1
+// num3 = num1 + num 2
+// sum = 0
+// if num3 %2 === 0, sum += num3 
+// num1 = num3
+// num2 = num3
+// num3 = num1 + num2
+
+while (fibonacci <= 400) {
+    
+}
