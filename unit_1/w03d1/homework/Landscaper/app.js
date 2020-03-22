df86e5d41284a6755ec4fb9a070786c1ea0751f3
YJ Lee <leeyuejia@gmail.com>
2020-03-22 20:42:00 +0800

    Added homework halfway

--- unit_1/w03d1/homework/Landscaper/app.js
+++ unit_1/w03d1/homework/Landscaper/app.js
@@ -8,12 +8,83 @@ const start = ()=> {
 }
 
 const showStatus = () => {
-    alert("You have " + teeth + " and " + "$" + money + " left");
+    alert("You have " + teeth + " teeth left and " + "$" + money + " left");
 }
+const showStatus2 = () => {
+    alert("You have " + teeth + " teeth left and " + " You have a siccsors and "+ "$" + money + " left");
+}
+
 
 const askForAction = () => {
     showStatus();
-    const choice = prompt('What do you want to do?', "use teeth / ");
-    if (choice ===)
+    const choice = prompt('What do you want to do?', "use teeth / buy sicssors($5) / restart");
+    if (choice === 'use teeth'){
+        alert('You have used a teeth to cut grass')
+        cutLawn()
+    } else if (choice === 'buy sicssors($5)') {
+        alert('You have spent $5 to buy a sicssors')
+        buySiccsors()
+    } else if (choice === 'restart') {
+        alert('thank you for cutting grass')
+        start();
+    }
+    };
+
+const askForAction2 = () => {
+        showStatus();
+        const choice = prompt('What do you want to do?', "use teeth / buy lawnmower($25) / use sicssors / restart");
+        if (choice === 'use teeth'){
+            alert('You have used a teeth to cut grass')
+            cutLawn()
+        } else if (choice === 'buy lawnmower($25)') {
+            alert('You have spent $25 to buy a lawnmower')
+            buyLawnmower()
+        } else if (choice === 'use sicssors') {
+            alert('You used a sicssors to cut grass')
+            cutMoreLawn()
+        } else if (choice === 'restart') {
+            alert('thank you for cutting grass')
+            start();
+        }
+        };
+        
+const askForAction3 = () => {
+    showStatus();
+    const choice = prompt('What do you want to do?', "use teeth / buy advance lawnmower($25) / use sicssors / restart");
+    if (choice === 'use teeth'){
+        alert('You have used a teeth to cut grass')
+        cutLawn()
+    } else if (choice === 'buy lawnmower($25)') {
+        alert('You have spent $25 to buy a lawnmower')
+        buyLawnmower()
+    } else if (choice === 'use sicssors') {
+        alert('You used a sicssors to cut grass')
+        cutMoreLawn()
+    } else if (choice === 'restart') {
+        alert('thank you for cutting grass')
+        start();
+    }
+    };
+        
+//ACTIONS FOR FUNCTION
+
+const cutLawn = () => {
+    teeth -=1
+    money ++
+    askForAction();
+};
+const buySiccsors = () => {
+    money -=5
+    askForAction2();
+}
+const cutMoreLawn = () => {
+    money +=5
+    askForAction2();
+}
+const buyLawnmower = () => {
+    money -=25
+    askForAction3();
+
 }
 
+start();
--- unit_1/w03d1/student_labs/pseudocode.js
+++ unit_1/w03d1/student_labs/pseudocode.js
@@ -16,33 +16,3 @@
 // let jamscoop = jam +=spoon.
 // add jamscoop on breadsliceB. 
 // let peanutbutterjellysandwich = breadsliceA + breadsliceB
-
-//QNS: Find all fibonacci sequence. if fibonacci numbers within 400000, all up the numbers
-// that are even numbers.
-
-//fibonacci number :
-// 0 + 1 = 1
-// 1 + 1 = 2
-// 1 + 2 = 3
-// 2 + 3 = 5
-// 3 + 5 = 8
-let firstNum = 0;
-let secondNum = 1;
-let thirdNum = firstNum + secondNum;
-let sum = 0;
-
-while (thirdNum <= 4000000) {
-    firstNum = secondNum
-    secondNum = thirdNum
-    thirdNum = firstNum + secondNum
-    if (thirdNum%2 ===0) {
-        sum +=thirdNum
-    }
-};
-
-console.log(sum);
-// if num3 %2 === 0, sum += num3 then 
-// num1 = num3
-// num2 = num3
-// num3 = num1 + num2
- 
