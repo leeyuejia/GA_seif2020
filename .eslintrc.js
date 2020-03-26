501fe53bdd5271039ae5034943e9e9da12f64276
Min Shan <min.shan@online-pajak.com>
2020-03-26 19:34:10 +0800

    add eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': ['warn', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['warn', 'single'],
+        'semi': ['warn', 'always'],
+        'object-property-newline': ['warn', { allowAllPropertiesOnSameLine: true }],
+        'comma-dangle': ['warn', {
+            'arrays': 'only-multiline',
+            'objects': 'only-multiline',
+            'imports': 'only-multiline',
+            'exports': 'only-multiline',
+        }],
+        'space-before-function-paren': 'warn',
+        'object-curly-spacing': ["warn", 'always']
+    }
+};
