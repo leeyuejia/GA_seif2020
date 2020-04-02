48c416f65d2ad06a6287643edc697421a62fd883
YJ Lee <leeyuejia@gmail.com>
2020-04-02 22:34:53 +0800

    DOM

--- unit_1/w04d3/instructor_examples/dom_intro/app.js
+++ unit_1/w04d3/instructor_examples/dom_intro/app.js
@@ -1,5 +1,5 @@
 console.log('stranger things are coming');
-//document.querySelector('h1').innerText = 'Welcome to the Upside Down';
+document.querySelector('h1').innerText = 'Welcome to the Upside Down';
 
 const image = document.querySelector('img');
 // console.log(image.style);
