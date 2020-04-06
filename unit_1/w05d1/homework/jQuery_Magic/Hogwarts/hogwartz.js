a1dd9e870290804346feb488955675d0e5a8e985
YJ Lee <leeyuejia@gmail.com>
2020-04-06 11:07:47 +0800

    added more elements

--- unit_1/w05d1/homework/jQuery_Magic/Hogwarts/hogwartz.js
+++ unit_1/w05d1/homework/jQuery_Magic/Hogwarts/hogwartz.js
@@ -4,9 +4,15 @@
 
 
   const $container = $('<div>').attr('id', 'container')
-  const $text = $('<h1>').text('Hogwarts')
-  
+  const $hogwarts = $('<h1>').text('Hogwarts')
+  const $james = $('<h2>').text('James')
+  const $house = $('<h3>').text('Gryffindor')
+  const $pet =$('<h4>').text('Owl')
+  const $wand = $('<h5>').text('Night Moon')
+  const $array = [$hogwarts, $james, $house, $pet, $wand]
   $(() => {
+      $('body').append($container)
+      $('#container').append($array)
 console.log($container)
-console.log($text)
+console.log($hogwarts)
   })
