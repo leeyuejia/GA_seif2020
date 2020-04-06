144edcef929cc38905f70a1262d54c3333a93bf8
YJ Lee <leeyuejia@gmail.com>
2020-04-06 10:55:56 +0800

    added elements

--- unit_1/w05d1/homework/jQuery_Magic/Hogwarts/hogwartz.js
+++ unit_1/w05d1/homework/jQuery_Magic/Hogwarts/hogwartz.js
@@ -2,6 +2,11 @@
     console.log('oops! I still have to link my jQuery properly!');
   } else {console.log('I did it! I linked jQuery and this js file!')};*/
 
-  $(() => {
 
+  const $container = $('<div>').attr('id', 'container')
+  const $text = $('<h1>').text('Hogwarts')
+  
+  $(() => {
+console.log($container)
+console.log($text)
   })
