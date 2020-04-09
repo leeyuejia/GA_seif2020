a9846bc04509329b86e1df9a037ccc5d4f521c7e
Eason Zhou <yinsheng@sph.com.sg>
2020-04-09 15:28:24 +0800

    upadte

--- unit_1/w05d3/instructor_examples/event_targeting/app.js
+++ unit_1/w05d3/instructor_examples/event_targeting/app.js
@@ -1,55 +1,58 @@
-console.log('Oh hey, I remember something like this')
+console.log('Oh hey, I remember something like this');
 
 
 let handOfCards = [];
 
 const clickCard = (card) => {
-
+    console.log('card is', card);
     // need to push something to handOfCards
     handOfCards.push(card);
     console.log('adding to', handOfCards);
+    console.log($(handOfCards[0]).children().eq(1).text());
 
     if (handOfCards.length === 2) {
         console.log('checking', handOfCards);
         console.log($(handOfCards[0]).children().eq(1).text());
         if($(handOfCards[0]).children().eq(1).text() === $(handOfCards[1]).children().eq(1).text()) {
-            console.log("we have matching cards");
+            console.log('we have matching cards');
             $(handOfCards[0]).hide('slow');
             $(handOfCards[1]).hide('slow');
             handOfCards = [];
         }
         else {
-            console.log("we dont have matching cards");
+            console.log('we dont have matching cards');
             $(handOfCards[0]).toggleClass('card-back');
             $(handOfCards[1]).toggleClass('card-back');
             handOfCards = [];
         }
     }
-}
+};
 
 $(() => {
-    //code runs when document is ready or dom is loaded
+    // code runs when document is ready or dom is loaded
     $('.card').on('click', (event) => {
-        console.log($(event.currentTarget));
+        console.log(event);
+        // console.log(event.currentTarget);
+        // console.log($(event.currentTarget));
         if($(event.currentTarget).hasClass('card-back')) {
             $(event.currentTarget).toggleClass('card-back');
             clickCard(event.currentTarget);
         }
-    })
+    });
 
-    // $('#card-1').on('click', () => {
+    // $('.card').on('click', () => {
     //     $('.card').toggleClass('card-back');
-    // })
+    // });
 
     // $('#card-2').on('click', () => {
     //     $('.card').toggleClass('card-back');
-    // })
+    // });
 
     // $('#card-3').on('click', () => {
     //     $('.card').toggleClass('card-back');
-    // })
+    // });
 
     // $('#card-4').on('click', () => {
     //     $('.card').toggleClass('card-back');
-    // })
-})
+    // });
+});
