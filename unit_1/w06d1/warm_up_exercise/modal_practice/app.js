fc3bd0275a5ee7dd5eff24f68e743430e739ca09
YJ Lee <leeyuejia@gmail.com>
2020-04-14 20:12:15 +0800

    Modal practice

--- unit_1/w06d1/warm_up_exercise/modal_practice/app.js
+++ unit_1/w06d1/warm_up_exercise/modal_practice/app.js
@@ -1,6 +1,25 @@
 // console.log($);
 // console.log('modal practice app.js is linked to this index.html');
 
-$( () => {
+
+
+
+
+$(() => {
+  const $openBtn = $('#openModal');
+  const $modal = $('#modal');
+  const $closeBtn = $('#close');
+
+  //Close button function
+  const closeModal = () => {
+    $modal.css('display', 'none');
+  }
+  //Open button function
+  const openModal = () => {
+    $modal.css('display', 'block');
+  }
+  $openBtn.on('click', openModal);
+  $closeBtn.on('click', closeModal);
+  setTimeout(openModal, 5000);
   //write your code here
 });
