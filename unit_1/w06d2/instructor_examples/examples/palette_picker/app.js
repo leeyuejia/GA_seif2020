9064ea27e28b3e8daa568debce75d6caa5c8cf3c
Eason Zhou <yinsheng@sph.com.sg>
2020-04-15 22:45:23 +0800

    add content

--- unit_1/w06d2/instructor_examples/examples/palette_picker/app.js
+++ unit_1/w06d2/instructor_examples/examples/palette_picker/app.js
@@ -1,23 +1,35 @@
-$(() => {
-    const getRandomColor = () => {
-        const red = Math.floor(Math.random()*256);
-        const green = Math.floor(Math.random()*256);
-        const blue = Math.floor(Math.random()*256);
-        return 'rgb('+red+','+green+','+blue+')';
-    }
+const $colorPalette = $('#color-palette');
+const $myPalette = $('#my-palette');
+const $generate = $('#generate');
 
-    for(let i = 0; i < 150; i++){
-        const $square = $('<div>').addClass('square').attr('id', 'square'+i).css('background', getRandomColor());
-        $('#colors').append($square);
-    }
-    $('.square').on('click', (event) => {
-        // console.log($('body').attr('css'));
-        const chosenBackgroundColor = $(event.currentTarget).css('background-color');
-        const $newDiv = $('<div>').addClass('square').css('background-color',chosenBackgroundColor);
-        $newDiv.appendTo('#choices');
-        // $(event.currentTarget).appendTo('#choices');
-        // console.log($(event.currentTarget));
-        // $('#choices').append(event.currentTarget);
-    });
+const addColor = event => {
+    const color = $(event.currentTarget).css('background-color');
 
-});
+    const $square = $('<div>');
+    $square.addClass('square');
+    $square.css('background-color', color);
+
+    $myPalette.append($square);
+};
+
+const randomColor = () => {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    return color;
+};
+
+const makePalette = () => {
+    $colorPalette.empty();
+
+    for (let i = 0; i < 150; i++) {
+        const $square = $('<div>')
+            .addClass('square')
+            .css('background-color', randomColor());
+        $colorPalette.append($square);
+    }
+    $('.square').on('click', addColor);
+};
+
+$generate.on('click', makePalette);
