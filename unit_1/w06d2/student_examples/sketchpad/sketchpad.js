31111bab040c8c5e749ae37e3c71922206285bcc
YJ Lee <leeyuejia@gmail.com>
2020-04-16 23:26:55 +0800

    student lab done

--- unit_1/w06d2/student_examples/sketchpad/sketchpad.js
+++ unit_1/w06d2/student_examples/sketchpad/sketchpad.js
@@ -1,2 +1,46 @@
-$(()=>{
+console.log('loaded and live')
+
+const width = 600;
+const height = 600;
+
+$(() => {
+    $('.container').css({ width: width + "px", height: height + "px" })
+
+    const randomColor = () => {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        return `rgb(${r},${g},${b})`
+    }
+
+    const calculateLength = (length, breadth, num) => {
+        return Math.floor(Math.sqrt((length * breadth) / num)
+        );
+    };
+
+    const changeColor = (event) => {
+        // $(event.currentTarget).addClass('pink');
+        $(event.currentTarget).css('background', randomColor());
+    }
+    const renderSquare = (num) => {
+        // console.log('squre is ' + calculateWidth(width, height, number));
+        $('.container').empty();
+        for (let i = 0; i < num; i++) {
+            $square = $('<div>').addClass('square').css({
+                width: calculateLength(width, height, num) + 'px',
+                height: calculateLength(width, height, num) + 'px'
+            })
+            $('.container').append($square);
+        }
+        $('.square').on('mousedown mouseover', changeColor);
+    };
+
+    $('form').on('submit', event => {
+        const inputValue = parseInt($('#input-box').val());
+
+        renderSquare(inputValue)
+        console.log(inputValue);
+        event.preventDefault()
+        $(event.currentTarget).trigger('reset');
+    })
 });
--- unit_1/w06d2/student_labs/morning/shape_clicker/js/app.js
+++ unit_1/w06d2/student_labs/morning/shape_clicker/js/app.js
@@ -1,3 +1,34 @@
 console.log('shape clicker js file is connected');
 
 //remember to set up your window onload!
+$(() => {
+    let clickNum = 0
+    const shapeArray = ['circle','square','triangle-down', 'octagon', 'heart']
+    const iteration = () => {
+        clickNum ++;
+        if (clickNum === 1) {
+            $(event.target).attr('class', shapeArray[0])
+        }
+        if (clickNum === 2) {
+            $(event.currentTarget).attr('class', shapeArray[1])
+        }
+        if (clickNum === 3) {
+            $(event.currentTarget).attr('class', shapeArray[2])
+        }
+        if (clickNum === 4) {
+            $(event.currentTarget).attr('class', shapeArray[3])
+        }
+        if (clickNum === 5) {
+            $(event.currentTarget).attr('class', shapeArray[4])
+        }
+        if (clickNum === 6) {
+            $(event.currentTarget).attr('class', shapeArray[5])
+        }
+        if (clickNum === 7) {
+            $(event.currentTarget).attr('class', 'triangle')
+            clickNum = 0;
+        }
+    }
+
+    $('.triangle').on('click', iteration)
+});
