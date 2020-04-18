94ac0c4979ca464a95a0f6428c72fc72efa1828e
YJ Lee <leeyuejia@gmail.com>
2020-04-18 10:29:14 +0800

    construction calculator

---
+++ unit_1/w06d2/homework/Calculator/calculator.js
@@ -0,0 +1,10 @@
+// create 3 array
+// let num = []
+// let num2 = []
+// let operator =
+// let sum = []
+// each number u click, u push the value to num,
+// click operator, then flatten array num and concaenate them into one variable (num1Sum)
+// number clicked after operator, push the value to num2 array. 
+// click equal, will flatten both array concaenate them and assign them into variable. then variable1 + operator + variable2
+// cancel function will empty the currentTarget
