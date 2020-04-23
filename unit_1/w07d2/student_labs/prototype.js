c5380157a4a1fe3270b83466a55774a4b67d4ca3
YJ Lee <leeyuejia@gmail.com>
2020-04-23 22:13:50 +0800

    prototypes and TDD

---
+++ unit_1/w07d2/student_labs/prototype.js
@@ -0,0 +1,48 @@
+// function Shape(name, sides, sideLength) {
+//     this.name = name;
+//     this.sides = sides;
+//     this.sideLength = sideLength;    
+//   }
+
+//   Shape.prototype.calcPerimeter = function () {
+//       return this.sideLength * this.sides;
+//   };
+
+//   const square = new Shape('square', 4, 5);
+
+//   const triangle = new Shape('triangle', 3,3);
+
+//   console.log(triangle.calcPerimeter());
+
+
+  //USING ES6 syntex
+  class shape {
+      constructor (name, sides, sideLength) {
+          this.name = name;
+          this.sides = sides;
+          this.sideLength = sideLength;
+      }
+      calcPerimeter () {
+          return this.sideLength * this.sides;
+      };
+  }
+
+  const newSquare = new shape('square', 4, 5);
+  const newTriangle = new shape ('triangle', 3, 3);
+
+  console.log(newSquare.calcPerimeter())
+  console.log(newTriangle.calcPerimeter());
+
+class Square extends shape {
+    constructor (sideLength) {
+        super('square', 4, sideLength)
+    }
+    calcArea () {
+        return this.sideLength ** 2;
+    }
+}
+
+const square = new Square(10);
+
+console.log(square.calcArea(10))
+console.log(square.calcPerimeter())
