303ea7a530b67be387ac70e91356621c6f3fae03
YJ Lee <leeyuejia@gmail.com>
2020-05-09 16:26:22 +0800

    Tip calculator express application created

--- unit_2/w09d1/homework/First_Express_HW/homework.js
+++ unit_2/w09d1/homework/First_Express_HW/homework.js
@@ -3,9 +3,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000
 
+//ALL FUNCTION
+const tip = (bill,tipRate) => {
+    return (tipRate / 100) * bill;
+}
+
 //ALL ROUTES
 app.get("/greeting/:name", (req,res) => {
     res.send(`Hello, ${req.params.name}`);
+})
+
+app.get("/tip/:total/:tippercentage", (req,res) => {
+    let bill = parseInt(req.params.total)
+    let tipRate = parseInt(req.params.tippercentage)
+    res.send("total bill is " + bill + " and the tip is " + tip(bill,tipRate));
 })
 
 
