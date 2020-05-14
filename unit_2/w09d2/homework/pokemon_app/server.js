7dc05047a19654069f8f9db8900135593fd170a8
Eason Zhou <yinsheng@sph.com.sg>
2020-05-14 20:25:39 +0800

    Connected my database, can see json in the browser

--- unit_2/w09d2/homework/pokemon_app/server.js
+++ unit_2/w09d2/homework/pokemon_app/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pokemon = require('./models/pokemon');
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
+});
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
 });
 
 app.listen(port, () => {
