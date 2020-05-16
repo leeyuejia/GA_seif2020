94dbc831a6eeb8f903231cb63f533052af30dff5
Eason Zhou <yinsheng@sph.com.sg>
2020-05-16 16:12:30 +0800

    add new files

--- unit_2/w10d1/instructor_examples/fruits/server.js
+++ unit_2/w10d1/instructor_examples/fruits/server.js
@@ -4,16 +4,24 @@ const app = express();
 const fruits = require('./models/fruits.js');
 
 //tells express to try to match requests with files in the directory called 'public'
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // near the top, around other app.use() calls
-// app.use(express.urlencoded({ extended: false }));
+// 1. request.body
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/fruits', (request, response) => {
-    response.render('index.ejs', {
-        allFruits: fruits
-    });
+// 2. middleware
+app.use((req, res, next) => {
+    console.log('Middleware 2');
+    next();
 });
+
+// INDEX route - show all the fruits
+app.get('/fruits', (req, res) => {
+    res.render('index.ejs', { allFruits: fruits });
+});
+
 
 // NEW route - put this above your show.ejs file
 app.get('/fruits/new', (req, res) => {
@@ -21,10 +29,18 @@ app.get('/fruits/new', (req, res) => {
 });
 
 // SHOW route
-app.get('/fruits/:indexOfFruitsArray', (request, response) => {
-    response.render('show.ejs', {
-        fruit: fruits[request.params.indexOfFruitsArray]
-    });
+app.get('/fruits/:index', (req, res) => {
+    res.render('show.ejs', { fruit: fruits[req.params.index] });
+});
+
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ // if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { // if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    res.redirect('/fruits');
 });
 
 app.listen(3000, () => {
--- unit_2/w10d1/student_labs/afternoon-command-line-lab/server.js
+++ unit_2/w10d1/student_labs/afternoon-command-line-lab/server.js
@@ -7,17 +7,34 @@ const port = 3000;
 
 // middleware
 // ?
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // data
 const dates = [
-  { name: "Bangor", created_at: Date.now() },
-  { name: "San Jose", created_at: Date.now() },
-  { name: "Bozeman", created_at: Date.now() }
+    { name: 'Bangor', created_at: Date.now() },
+    { name: 'San Jose', created_at: Date.now() },
+    { name: 'Bozeman', created_at: Date.now() }
 ];
 
 // get route
+app.get('/dates', (req, res) => {
+    res.send(dates);
+});
 
 // post route
+app.post('/dates', (req, res) => {
+    console.log('req.body is: ', req.body);
+    if (!req.body.name) { req.body.name = 'NO DATA'; }
+    req.body.created_at = Date.now();
+    dates.push(req.body);
+    console.log('all the dates data: ', dates);
+    res.redirect('/dates');
+});
 
+app.post('/products', (req, res) => {
+    console.log('req.body is: ', req.body);
+    res.redirect('/dates');
+});
 
 app.listen(port, () => console.log('Running on port: ', port));
