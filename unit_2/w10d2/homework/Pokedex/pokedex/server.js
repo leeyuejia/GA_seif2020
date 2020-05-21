82f65af9b310f49729f93693ea0f8a4296e939a9
YJ Lee <leeyuejia@gmail.com>
2020-05-21 14:52:55 +0800

    pokemon index.ejs page done

--- unit_2/w10d2/homework/Pokedex/pokedex/server.js
+++ unit_2/w10d2/homework/Pokedex/pokedex/server.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 8080
-
+const methodOverride = require('method-override')
 
 const data = require("./models/pokemon")
+const dataKeys = Object.keys(data)
 // require("./router/router.js")(app);
 
 // set view to ejs
@@ -13,13 +14,32 @@ app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"))
 //ALL FUNCTION
 
 const indexPage = (req,res) => {
     res.render("index.ejs", {data})
 }
+const newPage = (req,res) => {
+    res.render("new.ejs", {data, dataKeys})
+}
+
+//ALL ROUTES
 app.get("/pokemon", indexPage)
+
+app.get("/pokemon/new", newPage)
+
+// app.get("/pokemon:id", showPage)
+
+// app.get("/pokemon/:id/edit", editPage)
+
+// app.post("/pokemon", postPage)
+
+// app.put("/pokemon", putPage)
+
+// app.delete("/fruits/:index", deletePage)
+
+
 
 app.listen(port, () => {
     console.log("I am listening on port", port);
