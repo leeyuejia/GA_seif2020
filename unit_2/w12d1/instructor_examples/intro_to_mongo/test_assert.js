2abdb3fb6f423c02b14ebddf779748ae2e741afc
YJ Lee <leeyuejia@gmail.com>
2020-05-30 18:30:24 +0800

    doing task await/async

--- unit_2/w12d1/instructor_examples/intro_to_mongo/test_assert.js
+++ unit_2/w12d1/instructor_examples/intro_to_mongo/test_assert.js
@@ -1,4 +1,68 @@
-const assert = require('assert');
-assert.equal(1, 1);
+// const assert = require('assert');
+// assert.equal(1, 1);
+const fetch = require('node-fetch');
+// console.log('Hello world');
 
-console.log('Hello world');
+//// https://javascript.info/async-await#tasks task////
+//task 1
+const loadJson = async (url) => {
+        let result = await fetch(url)
+        if (result.status == 200) {
+            return result.json();
+        }
+        throw new Error (err.status)
+    }
+
+
+// function loadJson(url) {
+//     return fetch(url)
+//       .then(response => {
+//         if (response.status == 200) {
+//           return response.json();
+//         } else {
+//           throw new Error(response.status);
+//         }
+//       })
+//   }
+  
+  loadJson('https://mhw-db.com/charms')
+    .catch(console.log('error'))
+
+// // task 2
+
+// class HttpError extends Error {
+//     constructor(response) {
+//       super(`${response.status} for ${response.url}`);
+//       this.name = 'HttpError';
+//       this.response = response;
+//     }
+//   }
+//   const loadJson = async (url) => {
+//     let result = await fetch(url)
+//     if (result.status == 200) {
+//         let jsonResult = await result.json()
+//         return jsonResult
+//     }
+//     throw new HttpError(result)
+// }
+  
+//   // Ask for a user name until github returns a valid user
+//   async function demoGithubUser() {
+//     let name = await prompt("Enter a name?", "iliakan");
+  
+//     return loadJson(`https://api.github.com/users/${name}`)
+//       .then(user => {
+//         alert(`Full name: ${user.name}.`);
+//         return user;
+//       })
+//       .catch(err => {
+//         if (err instanceof HttpError && err.response.status == 404) {
+//           alert("No such user, please reenter.");
+//           return demoGithubUser();
+//         } else {
+//           throw err;
+//         }
+//       });
+//   }
+  
+//   demoGithubUser();
