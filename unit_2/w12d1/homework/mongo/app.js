b66764937a41f683b80536700620af6012cb6e70
Eason Zhou <yinsheng@sph.com.sg>
2020-06-02 21:30:52 +0800

    vampire homework and async/await

--- unit_2/w12d1/homework/mongo/app.js
+++ unit_2/w12d1/homework/mongo/app.js
@@ -13,10 +13,10 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'homework';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 // Use connect method to connect to the Server
-client.connect(function (err) {
+client.connect(async (err) => {
     assert.equal(null, err);
     console.log('Connected successfully to server');
 
@@ -25,6 +25,401 @@ client.connect(function (err) {
     /**********************************************************************
     Write Your Code Below
     **********************************************************************/
+    // try {
+    //     const result = await Vampires.insertMany(vampires);
+    //     assert.equal(vampires.length, result.insertedCount);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
 
+    // try {
+    //     const results = await Vampires.find({ gender: 'f' }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $gt: 500
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $lte: 150
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $ne: 210234
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $gt: 150,
+    //             $lt: 500
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         title: {
+    //             $exists: true
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $exists: false
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         title: {
+    //             $exists: true
+    //         },
+    //         victims: {
+    //             $exists: false
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $exists: true,
+    //             $gt: 1000
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         $or: [{
+    //             location: 'New York, New York, US'
+    //         }, {
+    //             location: 'New Orleans, Louisiana, US'
+    //         }]
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         $or: [{
+    //             loves: 'brooding'
+    //         }, {
+    //             loves: 'being tragic'
+    //         }]
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         $or: [{
+    //             victims: {
+    //                 $gt: 1000
+    //             }
+    //         }, {
+    //             loves: 'marshmallows'
+    //         }]
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         $or: [{
+    //             hair_color: 'red'
+    //         }, {
+    //             eye_color: 'green'
+    //         }]
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         loves: {
+    //             $in: [
+    //                 'appearing innocent', 
+    //                 'trickery', 
+    //                 'lurking in rotting mansions', 
+    //                 'R&B music'
+    //             ]
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         loves: {
+    //             $in: ['fancy cloaks'],
+    //             $nin: [
+    //                 'top hats', 
+    //                 'virgin blood', 
+    //             ]
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         loves: 'ribbons',
+    //         eye_color: {
+    //             $ne: 'brown'
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         location: {
+    //             $ne: 'Rome, Italy'
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         loves: {
+    //             $nin: [
+    //                 'fancy cloaks', 
+    //                 'frilly shirtsleeves', 
+    //                 'appearing innocent', 
+    //                 'being tragic', 
+    //                 'brooding'
+    //             ]
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const results = await Vampires.find({
+    //         victims: {
+    //             $lte: 200
+    //         }
+    //     }).toArray();
+    //     console.log(results);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+    
+    // try {
+    //     const result = await Vampires.updateOne({
+    //         name: 'Claudia'
+    //     }, {
+    //         $set: {
+    //             name: 'Eve',
+    //             portrayed_by: 'Tilda Swinton'
+    //         }
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.updateOne({
+    //         gender: 'm'
+    //     }, {
+    //         $set: {
+    //             name: 'Guy Man',
+    //             is_actually: 'were-lizard'
+    //         }
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.updateOne({
+    //         name: 'Guy Man'
+    //     }, {
+    //         $set: {
+    //             gender: 'f',
+    //         }
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.updateOne({
+    //         name: 'Guy Man'
+    //     }, {
+    //         $set: {
+    //             hates: ['clothes', 'jobs'],
+    //         }
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.updateOne({
+    //         name: 'Guy Man'
+    //     }, {
+    //         $push: {
+    //             hates: { $each: ['alarm clocks', 'jackalopes'] },
+    //         }
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.updateMany({
+    //         gender: 'f'
+    //     }, {
+    //         $set: {
+    //             gender: 'fems'
+    //         }
+    //     });
+    //     console.log(result.result);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    // try {
+    //     const result = await Vampires.deleteOne({
+    //         hair_color: 'brown'
+    //     });
+    //     assert.equal(1, result.result.n);
+    // } catch (err) {
+    //     assert.equal(null, err);
+    // } finally {
+    //     client.close();
+    // }
+
+    try {
+        const result = await Vampires.deleteMany({
+            eye_color: 'blue'
+        });
+        console.log(result.result);
+    } catch (err) {
+        assert.equal(null, err);
+    } finally {
+        client.close();
+    }
 });
 
--- unit_2/w12d2/instructor_examples/async-await/index.js
+++ unit_2/w12d2/instructor_examples/async-await/index.js
@@ -1,31 +1,90 @@
-// async function testAsync() {
-//   return 1;
+// display a world cup winner within 21 century 
+// every two seconds one after another in the chronicle order
+
+// 2002	 Brazil
+// 2006	 Italy
+// 2010	 Spain
+// 2014	 Germany
+// 2018	 France
+// const INTERVAL = 2000;
+
+// setTimeout(() => {
+//     console.log('2002	 Brazil');
+//     setTimeout(() => {
+//         console.log('2006	 Italy');
+//         setTimeout(() => {
+//             console.log('2010	 Spain');
+//             setTimeout(() => {
+//                 console.log('2014	 Germany');
+//                 setTimeout(() => {
+//                     console.log('2018	 France');
+//                 }, INTERVAL);
+//             }, INTERVAL);
+//         }, INTERVAL);
+//     }, INTERVAL);
+// }, INTERVAL);
+
+// // Promisify setTimeout
+// // the future state if it's fulfilled it calls resolve(value)
+// // if it's not fulfilled due to error exception, it calls reject(err);
+// const setTimeoutPromise = (duration, action, message) => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             action(message);
+//             resolve(); 
+//         }, duration);
+//     });
+// };
+
+// setTimeoutPromise(INTERVAL, console.log, '2002	 Brazil')
+//     .then(() => {
+//         return setTimeoutPromise(INTERVAL, console.log,'2006	 Italy');
+//     }).then(() => {
+//         return setTimeoutPromise(INTERVAL, console.log,'2010	 Spain');
+//     }).then(() => {
+//         return setTimeoutPromise(INTERVAL, console.log,'2014	 Germany');
+//     }).then(() => {
+//         return setTimeoutPromise(INTERVAL, console.log,'2018	 France');
+//     });
+
+// const main = async () => {
+//     await setTimeoutPromise(INTERVAL, console.log,'2002 Brazil');
+//     await setTimeoutPromise(INTERVAL, console.log,'2006	 Italy');
+//     await setTimeoutPromise(INTERVAL, console.log,'2010	 Spain');
+//     await setTimeoutPromise(INTERVAL, console.log,'2014	 Germany');
+//     await setTimeoutPromise(INTERVAL, console.log,'2018	 France');
+// };
+
+// main();
+
+// async function testAsync () {
+//     return 1;
 // }
 
-// function testPromise() {
-//   return Promise.resolve(2);
+// function testPromise () {
+//     return Promise.resolve(2);
 // }
 
-// function test() {
-//   return 3;
+// function test () {
+//     return 3;
 // }
 
 // testAsync()
-// .then(res => {
-//   console.log(`Result is ${res}`);
-// });
+//     .then(res => {
+//         console.log(`Result is ${res}`);
+//     });
 
 // testPromise()
-// .then(res => {
-//   console.log(`Result is ${res}`);
-// })
+//     .then(res => {
+//         console.log(`Result is ${res}`);
+//     });
 
 // test()
-// .then(res => {
-//   console.log(`Result is ${res}`);
-// });
+//     .then(res => {
+//         console.log(`Result is ${res}`);
+//     });
 
-console.log('AAAAAA');
+console.log('Very first message');
 
 function tryTestTimeout () {
     return new Promise((resolve, reject) => {
@@ -38,12 +97,12 @@ function tryTestTimeout () {
 }
 
 let testPromise = 
-new Promise((resolve, reject) => {
-    console.log('Before Timeout 2');
-    setTimeout(function () {
-        return resolve('SetTimeout 2 executed!');}, 1000);
-});
-
+    new Promise((resolve, reject) => {
+        console.log('Before Timeout 2');
+        setTimeout(function () {
+            return resolve('SetTimeout 2 executed!');
+        }, 1000);
+    });
 
 console.log('BREAK POINT 1 HERE ');
 
@@ -57,4 +116,15 @@ console.log('BREAK POINT 2 HERE');
 testPromise.then(res => {
     console.log(res);
 });
-console.log('BBBBBB');
+console.log('End of story');
+// console.log('Very first message');
+// setTimeout(() => {
+//     console.log('Time\'s up');
+// }, 100);
+// console.log('End of story');
+// console.log('End of story');
+// console.log('End of story');
+// console.log('End of story');
+// console.log('End of story');
+// console.log('End of story');
+// console.log('End of story');
