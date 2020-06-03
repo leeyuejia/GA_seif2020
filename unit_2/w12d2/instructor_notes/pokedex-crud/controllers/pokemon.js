013ea9023b79f5eb8a0cf61b1ff203793b2c2c71
Min Shan <min.shan@online-pajak.com>
2020-06-03 22:37:47 +0800

    render the new and show views

--- unit_2/w12d2/instructor_notes/pokedex-crud/controllers/pokemon.js
+++ unit_2/w12d2/instructor_notes/pokedex-crud/controllers/pokemon.js
@@ -2,4 +2,10 @@ module.exports = {
     getAll (req, res) {
         res.render('pokedex', { data: [] });
     },
+    create (req, res) {
+        res.render('new');
+    },
+    getOne (req, res) {
+        res.render('show', { data: { stats: [], type: [] } });
+    },
 };
--- unit_2/w12d2/instructor_notes/pokedex-crud/routes.js
+++ unit_2/w12d2/instructor_notes/pokedex-crud/routes.js
@@ -3,4 +3,6 @@ const { pokemonController } = require('./controllers');
 module.exports = (app) => {
     app.get('/', (req, res) => res.redirect('/pokemon'));
     app.get('/pokemon', pokemonController.getAll);
+    app.get('/pokemon/new', pokemonController.create);
+    app.get('/pokemon/:id', pokemonController.getOne);
 };
