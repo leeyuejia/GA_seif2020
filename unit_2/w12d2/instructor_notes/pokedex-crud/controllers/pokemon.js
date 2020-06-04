3cb12850787207071ae39c04a8d5778496af8b99
Min Shan <min.shan@online-pajak.com>
2020-06-04 21:50:49 +0800

    Add more code for CRUD

--- unit_2/w12d2/instructor_notes/pokedex-crud/controllers/pokemon.js
+++ unit_2/w12d2/instructor_notes/pokedex-crud/controllers/pokemon.js
@@ -1,11 +1,19 @@
+const pokemonRepository = require('../repositories/pokemonRepository');
+
 module.exports = {
-    getAll (req, res) {
-        res.render('pokedex', { data: [] });
+    async getAll (req, res) {
+        const pokemon = await pokemonRepository.getAll();
+        res.render('pokedex', { data: pokemon });
     },
     create (req, res) {
         res.render('new');
     },
-    getOne (req, res) {
-        res.render('show', { data: { stats: [], type: [] } });
+    async getOne (req, res) {
+        try {
+            const pokemon = await pokemonRepository.getOne(req.body);
+            res.render('show', { data: { stats: [], type: [] } });
+        } catch (err) {
+            res.send('err happened: ', err.message);
+        }
     },
 };
