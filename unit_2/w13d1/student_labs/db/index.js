e5dcd7afaebca75943270fd46c8de870d1b12e50
Min Shan <min.shan@online-pajak.com>
2020-06-12 22:43:13 +0800

    setup basic files

---
+++ unit_2/w13d1/student_labs/db/index.js
@@ -0,0 +1,21 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGO_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'homework';
+const COLLECTIONS = {
+    LOGS: 'logs',
+};
+
+const client = new MongoClient(MONGO_URL, { useUnifiedTopology: true });
+
+module.exports = {
+    async connect () {
+        const connection = await client.connect();
+        console.log('Connected to MongoDB');
+        const db = connection.db(DB_NAME);
+        this.logs = db.collection(COLLECTIONS.LOGS);
+    },
+    disconnect () {
+        return client.close();
+    },
+};
