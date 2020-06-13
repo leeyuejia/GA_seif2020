7b2c3d3f3509e4bff69d51a574e692a57d997de1
Min Shan <min.shan@online-pajak.com>
2020-06-13 14:21:37 +0800

    Add formatted date to logs

--- unit_2/w13d1/student_labs/controllers/logsController.js
+++ unit_2/w13d1/student_labs/controllers/logsController.js
@@ -1,10 +1,15 @@
 const logsRepository = require('../repositories/logsRepository');
-const logsValidator = require('../validators/logsValidator');
-const ValidationError = require('../exceptions/ValidationError');
+const moment = require('moment');
+
+const addFormattedDate = logs => logs.map(log => {
+    if (log.date) log.formattedDate = moment(log.date).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    return log;
+});
 
 module.exports = {
     async getAll (req, res) {
         const logs = await logsRepository.getAll(); // retrieved log from db
+        addFormattedDate(logs);
         res.render('logs/index', { logs });
     },
     getForm (req, res) {
