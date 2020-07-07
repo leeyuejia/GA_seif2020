a15308e7d0e212aecaf5fa9b58a2fd9dc23d6cf0
YJ Lee <leeyuejia@gmail.com>
2020-07-07 22:26:40 +0800

    100 globin and react state form

--- unit_3/w14d2/instructor_examples/react_state_store/app.js
+++ unit_3/w14d2/instructor_examples/react_state_store/app.js
@@ -89,92 +89,217 @@
 
 console.log(products);
 
+
+////////// RENDER DYNAMICALLY ///////
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       products: products,
-      name: "",
-      price: 0,
-      description: "Describe this item"
-    };
+      value: ''
+    }
   }
-  // handleChange = event => {
-  //   console.log(event.target.value);
-  //   this.setState({ value: event.target.value });
-  // };
-
   handleChange = event => {
-    this.setState({ [event.target.id]: event.target.value });
+    console.log(event.target.id);
+    this.setState({ [event.target.id]: event.target.value }, () => {
+      console.log(this.state)
+    });
   };
 
-  handleChangeName = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleChangePrice = event => {
-    this.setState({ price: event.target.value });
-  };
-
-  handleChangeDescription = event => {
-    this.setState({ description: event.target.value });
-  };
-
-  handleSubmit = event => {
+    handleSubmit = event => {
     event.preventDefault();
     const newItem = {
       name: this.state.name,
       price: this.state.price,
       description: this.state.description
-    };
-
-    this.setState({
-      products: [newItem, ...this.state.products],
-      name: "",
-      price: 0,
-      description: "Describe this item"
-    });
+    }
+    this.setState ({
+      products: [
+        newItem , 
+        ...this.state.products
+      ],
+      price: '',
+      description: '',
+      name: ''
+    })
   };
 
+  /// cannot use .forEach as it returns undefined
   render() {
     return (
       <div>
-        <h1> Big Time Shopping </h1>
-        <form onSubmit={this.handleSubmit}>
+        <h1>Big Time Shopping</h1>
+        <h2> Prview our new item</h2>
+        <h3>{this.state.name}</h3>
+        <h3>{this.state.price}</h3>
+        <h3>{this.state.description}</h3>
+        <form onSubmit = {this.handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChangeName}
-            id="name"
-            placeholder="name of product"
-          />
-          <br />
+          <input type="text" value={this.state.name} onChange={this.handleChange} id="name" placeholder = 'name of product'/>
           <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            value={this.state.price}
-            onChange={this.handleChangePrice}
-            id="price"
-          />
+          <input type="number" value={this.state.price} onChange={this.handleChange} id="price" />
           <br />
           <label htmlFor="description">Description</label>
-          <input
-            type="textarea"
-            value={this.state.description}
-            onChange={this.handleChangeDescription}
-            id="description"
-          />
-          <input type="submit" />
+          <input type="textarea" value={this.state.description} onChange={this.handleChange} id="description" />
+          <input type = "submit"/>
         </form>
-        {this.state.products.map(item => (
-          <li>
-            {item.name} {item.price}
-          </li>
-        ))}
+
+        <ul>
+          {this.state.products.map(product =>
+            <li>{product.name} ____ SGD{product.price} ___{product.description}</li>)}
+        </ul>
+
       </div>
-    );
+    )
   }
 }
+
+class Greet extends React.Component {
+  render() {
+    this.props.Name = "superman"
+    return (
+      <div>
+        hello {this.props.Name}, a.k.a {this.props.heroName}
+        <p>{this.props.children}</p>
+      </div>
+    )
+  }
+}
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Greet Name="Bruce" heroName="Batman">
+//         </Greet>
+//       </div>
+//     )
+//   }
+// }
+////////// RENDER DYNAMICALLY ///////
+
+
+////////// ADDING EVENT HANDLER ///////
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       price: 8,
+//     }
+//   }
+//   clickHandler = async () => {
+//     /// to change the price value, 
+//     this.state.price = 10;
+//     console.log(this.state.price)
+//     /// OR
+//     await this.setState({price: this.state.price +2})
+//     console.log(this.state.price)
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick ={this.clickHandler} >click me!</button>
+//         Hello World
+//         <p>{this.state.price}</p>
+//       </div>
+//     )
+//   }
+// }
+
+////////// ADDING EVENT HANDLER ///////
+
+
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       products: products,
+//       name: "",
+//       price: 0,
+//       description: "Describe this item"
+//     };
+//   }
+// handleChange = event => {
+//   console.log(event.target.value);
+//   this.setState({ value: event.target.value });
+// };
+
+//   handleChange = event => {
+//     this.setState({ [event.target.id]: event.target.value });
+//   };
+
+//   handleChangeName = event => {
+//     this.setState({ name: event.target.value });
+//   };
+
+//   handleChangePrice = event => {
+//     this.setState({ price: event.target.value });
+//   };
+
+//   handleChangeDescription = event => {
+//     this.setState({ description: event.target.value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     const newItem = {
+//       name: this.state.name,
+//       price: this.state.price,
+//       description: this.state.description
+//     };
+
+//     this.setState({
+//       products: [newItem, ...this.state.products],
+//       name: "",
+//       price: 0,
+//       description: "Describe this item"
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1> Big Time Shopping </h1>
+//         <form onSubmit={this.handleSubmit}>
+//           <label htmlFor="name">Name</label>
+//           <input
+//             type="text"
+//             value={this.state.name}
+//             onChange={this.handleChangeName}
+//             id="name"
+//             placeholder="name of product"
+//           />
+//           <br />
+//           <label htmlFor="price">Price</label>
+//           <input
+//             type="number"
+//             value={this.state.price}
+//             onChange={this.handleChangePrice}
+//             id="price"
+//           />
+//           <br />
+//           <label htmlFor="description">Description</label>
+//           <input
+//             type="textarea"
+//             value={this.state.description}
+//             onChange={this.handleChangeDescription}
+//             id="description"
+//           />
+//           <input type="submit" />
+//         </form>
+//         {this.state.products.map(item => (
+//           <li>
+//             {item.name} {item.price}
+//           </li>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
