3f49edcdef2516ec20040e66bfccb184a04a9207
angrylobster <min.shan@online-pajak.com>
2020-07-18 17:30:02 +0800

    fix server.js for testing

--- unit_4/w19d3/instructor_notes/todo-backend/server.js
+++ unit_4/w19d3/instructor_notes/todo-backend/server.js
@@ -5,10 +5,11 @@ const db = require('./db');
 
 app.use(express.urlencoded({ extended: false }));
 
-db.connect();
-
 require('./router')(app);
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+    db.connect();
+    app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+}
 
 module.exports = app;
